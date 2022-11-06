3186d8b054533fb42bd9a6ce765ddef49ddb4716
dianaklr <diana.klr148@gmail.com>
2022-11-06 17:43:27 +0200

    date

---
+++ src/app/components/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { User } from '../models/User';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  user: User[] = [];
+  constructor(private httpClient:HttpClient) {
+    this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe(value => this.user = value)
+    ;
+
+  }
+
+
+
+
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,6 @@
+export class User{
+  // @ts-ignore
+  id:number ;
+  // @ts-ignore
+  name: string  ;
+}
